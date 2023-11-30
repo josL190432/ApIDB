d1a399d1d82a060d88cd4b43b713b8b5534d579e
josL190432 <190432@utags.edu.mx>
2023-11-30 17:58:36 -0600

    26/11/23 190432

--- model/usuario.js
+++ model/usuario.js
@@ -2,19 +2,19 @@ const { model, Schema } = require("mongoose");
 
 const UsuarioSchema = new Schema({
   nombre: { type: String, required: true },
-  ApeS: { type: String, required: true },
-  Email: { type: String, required: true, unique: true },
-  NTel:{type:String},
-  Pas: { type: String, required: true },
-  CPol: { type: String},
-  MPa:{
-    Ntar: { type:Number },
-    NDue:{type:String},
-    FVe:{type:String}
-  },
-  FRe:{type:String},
-  Tus:{type:String},
- Img: { type:String }
+  // ApeS: { type: String, required: true },
+  //   Email: { type: String, required: true, unique: true },
+  //   NTel:{type:String},
+  //   Pas: { type: String, required: true },
+  //   CPol: { type: String},
+  //   MPa:{
+  //     Ntar: { type:Number },
+  //     NDue:{type:String},
+  //     FVe:{type:String}
+  //   },
+  //   FRe:{type:String},
+  //   Tus:{type:String},
+  // Img: { type:String }
 });
 
 module.exports = model("Usuario", UsuarioSchema);
